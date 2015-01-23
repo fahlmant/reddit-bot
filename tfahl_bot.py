95a9cfdb0d7c3fd4e659bd0c3b2014b978a8ef96
fahlmant <fahlmantaylor@gmail.com>
2015-01-23 07:38:30 +0100

    Almost fully functional reply bot

--- tfahl_bot.py
+++ tfahl_bot.py
@@ -1,13 +1,31 @@
+#Newbie Bot created by pizzamanzoo
 import time
 import praw 
 
-r = praw.Reddit("PRAW bot by /u/pizzamanzoo v 1.0")
+#Set the message for reddit
+r = praw.Reddit("Newbie Bot for newbie_bot_test by /u/pizzamanzoo v 1.0")
 
+#login with u/p combo
 r.login()
+#Initialize list of posts
 already_done = []
 
+#Words the bot will look for
+prawWords['test']
+
+
 while True:
-    subreddit = r.get_subreddit('scrolls')
-    print "Hello"
-#    for submission in subreddit.get_hot(limit=10):
+    #Set the subreddit name here
+    subreddit = r.get_subreddit('newbie_bot_test')
+    #looks at the 10 newest posts
+    for submission in subreddit.get_new(limit=10):
+        #Looks to see if words are in each submission
+        op_test = submission.selftext.lower()
+        has_raw = any(string in op_text for string in prawWords)
+        #If the words are there, post reply
+        if submission.id not in already_done and has_praw:
+            #post reply
+
+        time.sleep(2000)
+
     
